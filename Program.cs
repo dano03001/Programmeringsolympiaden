d30146f34441f3dc94f5db8d76fd5a7756571141
dano03001 <70076208+dano03001@users.noreply.github.com>
2020-11-27 08:45:24 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int p = 13;
+            int a = 7;
+
+            Console.Write("Äpplen ? " );
+            int antala = int.Parse(Console.ReadLine());
+            Console.Write("Päron ? ");
+            int antalp = int.Parse(Console.ReadLine());
+
+            int asum = antala * a;
+            int psum = antalp * p;
+
+            if (asum > psum)
+            {
+                Console.WriteLine("Svar: Axel");
+            }
+            else if (asum < psum)
+            {
+                Console.WriteLine("Svar: Petra");
+            }
+            else
+                Console.WriteLine("Svar: lika");
+        }
+    }
+}
